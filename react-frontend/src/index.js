15a0a25041b775402db4af89d248d0e0d4cd2ce5
Stephen <becker.stephen@gmail.com>
2020-11-22 14:24:42 -0500

    Add popup component to the item card - added new dependencies for icons and functionality -- material UI, semantic-ui-react, semantic-ui-css Extracted mock data generation functions to their own file for reuse Removed unused icons from manifest.json

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import './App.css';
 import EditMenu from './components/EditMenu';
