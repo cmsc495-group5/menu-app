d773e5e163613dda8f966b5bbf78ba2bc541aadc
brian <brian@mymugsandkisses.com>
2020-11-23 22:31:28 +0000

    first start of adding qr code generation

--- src/main/java/com/resturant/menu/models/Table.java
+++ src/main/java/com/resturant/menu/models/Table.java
@@ -2,7 +2,17 @@ package com.resturant.menu.models;
 
 import org.bson.codecs.pojo.annotations.BsonProperty;
 import org.springframework.data.annotation.Id;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
 
 @Document(collection= "tables")
 public class Table {
@@ -34,8 +44,11 @@ public class Table {
     }
 
     public String getQrCode() {
-        //@TODO implment qr code logic
-        return "aaaaa";
+        // @TODO inject from properties file and figure out what the url will be
+        String tableBaseUrl = "http://localhost:8080/order/table/";
+        String url = tableBaseUrl + id;
+        
+        return getQRCodeImage(url, 600, 600);
     }
     
     public String getUpdated() {
@@ -44,5 +57,21 @@ public class Table {
 
     public void setUpdated(String updated) {
         this.updated = updated;
+    }
+    
+    private String getQRCodeImage(String text, int width, int height) {
+        try {
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+            
+            ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+            byte[] pngData = pngOutputStream.toByteArray();
+            
+            String encoded = Base64.getEncoder().encodeToString(pngData);
+            return "data:image/png;base64," + encoded;
+        } catch (Exception e) {
+            return "";
+        }
     }
 }
