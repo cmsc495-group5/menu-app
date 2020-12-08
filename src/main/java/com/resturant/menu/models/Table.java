7289181a223770cbb9bd5c078d51742f0d13c260
brian <brian@mymugsandkisses.com>
2020-12-08 03:57:58 +0000

    add qr code logic

--- src/main/java/com/resturant/menu/models/Table.java
+++ src/main/java/com/resturant/menu/models/Table.java
@@ -1,15 +1,11 @@
 package com.resturant.menu.models;
 
+import net.glxn.qrgen.QRCode;
+
 import org.bson.codecs.pojo.annotations.BsonProperty;
 import org.springframework.data.annotation.Id;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
 
 import java.io.ByteArrayOutputStream;
 import java.util.Base64;
@@ -21,7 +17,8 @@ public class Table {
     String label;
     String updated;
     
-    public Table() {}
+    //Reading from application.properties would be a good next step
+    private final String tableBaseUrl = "http://localhost:8080/order/table/";
     
     public Table(String label) {
         this.label = label;
@@ -44,9 +41,8 @@ public class Table {
     }
 
     public String getQrCode() {
-        // @TODO inject from properties file and figure out what the url will be
-        String tableBaseUrl = "http://localhost:8080/order/table/";
         String url = tableBaseUrl + id;
+        System.out.println(url);
         
         return getQRCodeImage(url, 600, 600);
     }
@@ -61,15 +57,16 @@ public class Table {
     
     private String getQRCodeImage(String text, int width, int height) {
         try {
-            QRCodeWriter qrCodeWriter = new QRCodeWriter();
-            BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+            ByteArrayOutputStream stream = QRCode
+            .from(text)
+            .withSize(width, height)
+            .stream();
             
-            ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
-            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
-            byte[] pngData = pngOutputStream.toByteArray();
+            byte[] pngData = stream.toByteArray();
             
             String encoded = Base64.getEncoder().encodeToString(pngData);
-            return "data:image/png;base64," + encoded;
+            String img = "data:image/jpeg;base64," + encoded;
+            return img;
         } catch (Exception e) {
             return "";
         }
