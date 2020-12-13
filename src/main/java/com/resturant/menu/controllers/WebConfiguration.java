a33f5881b6ca5800df19ce23dfba90bd8f7c9401
Stephen <becker.stephen@gmail.com>
2020-12-13 18:15:21 -0500

    Add configuration to route the menu on deployed servers (after running 'npm run-script build' to deploy static files  needed to allow QR codes to work without going through the base path

---
+++ src/main/java/com/resturant/menu/controllers/WebConfiguration.java
@@ -0,0 +1,18 @@
+package com.resturant.menu.controllers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/admin")
+                .setViewName("forward:/");
+        registry.addViewController("/employee")
+                .setViewName("forward:/");
+        registry.addViewController("/menu")
+                .setViewName("forward:/");
+    }
+}
